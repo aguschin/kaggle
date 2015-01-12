fb7dfbd789005d36f6160c1ef78f082e292377a6
aguschin <1aguschin@gmail.com>
2015-01-12 15:42:18 +0300

    Create rain_crps.py

---
+++ rain_crps.py
@@ -0,0 +1,35 @@
+thresholds = [i for i in range(70)]
+
+def heavyside(actual):
+    return [1 if t >= actual else 0 for t in thresholds]
+
+def is_cdf_valid(case):
+    if case[0] < 0 or case[0] > 1:
+        return False
+    for i in xrange(1, len(case)):
+        if case[i] > 1 or case[i] < case[i-1]:
+            return False
+    return True
+
+def calc_crps(predictions, actuals):
+    #some vector algebra for speed
+    obscdf = np.array([heavyside(i) for i in actuals])
+    crps = mean(mean((predictions - obscdf) ** 2))
+    return crps
+
+def CRPS(predictions, actuals):
+    
+    check = True
+    '''
+    for p in predictions :
+        if is_cdf_valid(p) == False : 
+            print 'something wrong with your prediction'
+            check = False
+            break
+    '''
+    if check == True : 
+         return calc_crps(predictions, actuals)
+         
+###example of usage
+predictions = np.ones((train.shape[0],70))
+print CRPS(predictions, train.Expected)
